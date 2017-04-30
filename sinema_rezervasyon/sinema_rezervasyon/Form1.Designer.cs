895ed2634fc0a9145dcccf471473fe92d3094725
Tugrul Can <tugrulcansollu@gmail.com>
2017-04-30 22:15:12 +0300

    Buttons created automatically

---
+++ sinema_rezervasyon/sinema_rezervasyon/Form1.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace sinema_rezervasyon
+{
+    partial class frm_RezervasyonListe
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_RezervasyonListe
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(741, 480);
+            this.Name = "frm_RezervasyonListe";
+            this.Text = "Rezervasyonlar";
+            this.Load += new System.EventHandler(this.frm_RezervasyonListe_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ sinema_rezervasyon/sinema_rezervasyon/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinema_rezervasyon
+{
+    public partial class frm_RezervasyonListe : Form
+    {
+        public frm_RezervasyonListe()
+        {
+            InitializeComponent();
+        }
+
+        private void frm_RezervasyonListe_Load(object sender, EventArgs e)
+        {
+            butonlariYarat();
+            
+        }
+
+        private void butonlariYarat()
+        {
+            Point start_location = new Point() { X = 10, Y = 40 };
+            Size button_size = new Size(40, 40);
+
+            for (int i = 0; i < 5; i++)
+            {
+                start_location.Y += 60;
+                start_location.X = 10;
+                for (int j = 0; j < 12; j++)
+                {
+                    int musteri_no = ((i * 12) + j+1);
+
+                    Button b = new Button() {
+                        Location = start_location,
+                        Name = "btn_musteri_" + musteri_no.ToString(),
+                        Text = musteri_no.ToString(),
+                        Size = button_size,
+                        BackColor = Color.Chartreuse
+                    };
+                    b.Click += new System.EventHandler(this.btn_musteri_n_Click);
+
+                    this.Controls.Add(b);
+                    start_location.X += 60;
+                }
+            }
+        }
+
+        private void btn_musteri_n_Click(object sender, EventArgs e)
+        {
+            Button b = (Button) sender;
+            b.BackColor = Color.Red;
+
+        }
+    }
+}
